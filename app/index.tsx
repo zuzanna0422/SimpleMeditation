61a1e096559e58ff5df91e5d62db567956890386
Zuzanna Cholewa <zuzanna.cholewa@gmail.com>
2024-10-15 09:42:33 +0200

    start

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { View, Text, ImageBackground } from 'react-native'
+import React from 'react'
+
+import beachImage from '@/assets/meditation-images/beach.webp';
+const App = () => {
+  return (
+    <View className='flex-1'>
+        <ImageBackground 
+        source={beachImage} 
+        resizeMode='cover' 
+        className='flex-1'>
+
+      <Text>App</Text>
+      </ImageBackground>
+    </View>
+  )
+}
+
+export default App;
