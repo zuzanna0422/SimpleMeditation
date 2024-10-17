2d765032dec585f98572ce657d3cff9b16dac6a8
Zuzanna Cholewa <zuzanna.cholewa@gmail.com>
2024-10-17 08:41:19 +0200

    style changed to react native ones

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,33 @@
-import { View, Text, ImageBackground } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet } from 'react-native'
 import React from 'react'
 
 import beachImage from '@/assets/meditation-images/beach.webp';
 const App = () => {
   return (
-    <View className='flex-1'>
+    <View style={styles.container}>
         <ImageBackground 
         source={beachImage} 
         resizeMode='cover' 
-        className='flex-1'>
+        style={styles.imageBackground}>
 
-      <Text>App</Text>
+      <Text style={styles.text}>App</Text>
       </ImageBackground>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  imageBackground: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 20,
+  }
+});
 export default App;
